ba565eb82004a1a765c2eed47695f5df4ce617bc
shivenrshukla <shivenshukla297@gmail.com>
2026-04-26 17:12:50 +0530

    "Index updated"

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Preview app is running!");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>PreviewOps Environment #3 is Live!</h1>');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://0.0.0.0:${port}`);
+});
